427cf609b41fa67953e8e4d15965d6522f706c2f
mk-pmb <mk-pmb@users.noreply.github.com>
2022-11-02 21:36:07 +0100

    Migrate anno list fetcher to api22

---
+++ src/api22.js
@@ -0,0 +1,50 @@
+﻿/* -*- tab-width: 2 -*- */
+'use strict';
+/* eslint-env browser */
+
+const axios = require('axios');
+
+function ucFirst(s) { return s.slice(0, 1).toUpperCase() + s.slice(1); }
+
+const supportedHttpMethods = [
+  'get',
+  'post',
+];
+
+
+const defaultAxiosOpts = {
+  // maxRedirects: 0, // doesn't work in Firefox anyway
+  withCredentials: 1, // without it, xhr won't set cookies for CORS
+};
+
+
+const EX = function apiFactory(state) {
+  const {
+    annoEndpoint,
+  } = state;
+
+  const api = {
+  };
+
+  async function annoEndpointRequest(method, subUrl, data) {
+    if (!subUrl) { throw new Error('No endpoint sub URL given'); }
+    return axios({
+      ...defaultAxiosOpts,
+      method,
+      url: annoEndpoint + subUrl,
+      data,
+    });
+  }
+
+  supportedHttpMethods.forEach(function add(method) {
+    // Define aepGet, aepPost etc.:
+    api['aep' + ucFirst(method)] = annoEndpointRequest.bind(null, method);
+  });
+
+  return api;
+};
+
+
+
+// Object.assign(EX, {});
+module.exports = EX;
