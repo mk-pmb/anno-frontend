033796a0636d3a6ebc80261c009f01ddcec8a68e
mk-pmb <mk-pmb@users.noreply.github.com>
2022-12-06 10:09:01 +0100

    Refactor draft action button handler.

--- src/components/anno-drafts-panel/downloadAndRestoreDraft.js
+++ src/components/anno-drafts-panel/downloadAndRestoreDraft.js
@@ -6,17 +6,18 @@ const api22 = require('../../api22.js');
 const eventBus = require('../../event-bus.js');
 
 
-const EX = async function downloadAndRestoreDraft(fileName) {
+const EX = async function downloadAndRestoreDraft(meta) {
+  const { filename } = meta;
   const panel = this;
   const { statusMsg } = panel.$refs;
   // const { l10n } = panel;
-  statusMsg.setMsg({ text: '⏳ ' + fileName });
+  statusMsg.setMsg({ text: '⏳ ' + filename });
 
   const store = panel.$store;
   let draftData;
   try {
     draftData = await api22(store.state).endpointRequest('draftStore',
-      'GET', fileName);
+      'GET', filename);
   } catch (apiFail) {
     console.debug(EX.name, { apiFail });
     statusMsg.setMsg({ severity: 'fail', text: '❌ ' + apiFail });
