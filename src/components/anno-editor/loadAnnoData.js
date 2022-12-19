488eafd8e3d2810896a8f31fab63c341c08195b8
mk-pmb <mk-pmb@users.noreply.github.com>
2022-12-19 09:34:21 +0100

    New unified interface editor.loadAnnoData

---
+++ src/components/anno-editor/loadAnnoData.js
@@ -0,0 +1,59 @@
+// -*- coding: utf-8, tab-width: 2 -*-
+'use strict';
+
+
+const decideTargetForNewAnno = require('./decideTargetForNewAnno.js');
+const targetRelatedness = require('./targetRelatedness.js');
+
+
+function jsonDeepCopy(x) { return JSON.parse(JSON.stringify(x)); }
+
+
+const EX = function loadAnnoData(origAnno) {
+  const editor = this;
+  const { commit, state } = editor.$store;
+  const anno = jsonDeepCopy(origAnno);
+  const cfgTgt = decideTargetForNewAnno(state);
+  anno.target = EX.adjustMultiTarget(cfgTgt, anno.target);
+  const tgtAdj = anno.target.primaryTargetAdjustHint;
+  commit('RESET_ANNOTATION');
+  commit('REPLACE_ANNOTATION', anno);
+
+  const { l10n } = editor;
+  editor.$refs.targetAdjustedMsg.setMsg(null,
+    (tgtAdj && l10n('target_adjusted_' + tgtAdj)));
+};
+
+
+Object.assign(EX, {
+
+  adjustMultiTarget(cfgTgt, origTgt) {
+    let primary;
+    let adjusted = '';
+    const matchesConfigTarget = targetRelatedness.sameAsConfigTarget(cfgTgt);
+
+    function isAdditionalTarget(tgt, idx) {
+      if (!tgt) { return false; }
+      if (primary) { return true; }
+      if (matchesConfigTarget(tgt)) {
+        primary = tgt;
+        if (idx !== 0) { adjusted = 'hoisted'; }
+        return false;
+      }
+      return true;
+    }
+    const additional = [].concat(origTgt).filter(isAdditionalTarget);
+
+    if (!primary) {
+      primary = cfgTgt;
+      adjusted = 'added';
+    }
+    const targets = [primary, ...additional];
+    targets.primaryTargetAdjustHint = adjusted;
+    return targets;
+  },
+
+});
+
+
+module.exports = EX;
