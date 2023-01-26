376a411ca3e2d42833b40e7416ce1907f8bb78fa
mk-pmb <mk-pmb@users.noreply.github.com>
2023-01-26 20:06:13 +0100

    Anno-Editor: Factor out adjustMultiTarget

--- src/components/anno-editor/loadAnnoData.js
+++ src/components/anno-editor/loadAnnoData.js
@@ -1,9 +1,8 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 'use strict';
 
-
+const adjustMultiTarget = require('./adjustMultiTarget.js');
 const decideTargetForNewAnno = require('./decideTargetForNewAnno.js');
-const targetRelatedness = require('./targetRelatedness.js');
 
 
 function jsonDeepCopy(x) { return JSON.parse(JSON.stringify(x)); }
@@ -14,7 +13,7 @@ const EX = function loadAnnoData(origAnno) {
   const { commit, state } = editor.$store;
   const anno = jsonDeepCopy(origAnno);
   const cfgTgt = decideTargetForNewAnno(state);
-  anno.target = EX.adjustMultiTarget(cfgTgt, anno.target);
+  anno.target = adjustMultiTarget(cfgTgt, anno.target);
   const tgtAdj = anno.target.primaryTargetAdjustHint;
   commit('RESET_ANNOTATION');
   commit('REPLACE_ANNOTATION', anno);
@@ -25,37 +24,6 @@ const EX = function loadAnnoData(origAnno) {
 
   editor.redisplayZoneEditorSvg();
 };
-
-
-Object.assign(EX, {
-
-  adjustMultiTarget(appCfg, origTgt) {
-    let primary;
-    let adjusted = '';
-    const matchesConfigTarget = targetRelatedness.sameAsConfigTarget(appCfg);
-
-    function isAdditionalTarget(tgt, idx) {
-      if (!tgt) { return false; }
-      if (primary) { return true; }
-      if (matchesConfigTarget(tgt)) {
-        primary = tgt;
-        if (idx !== 0) { adjusted = 'hoisted'; }
-        return false;
-      }
-      return true;
-    }
-    const additional = [].concat(origTgt).filter(isAdditionalTarget);
-
-    if (!primary) {
-      primary = matchesConfigTarget.getConfiguredTarget();
-      adjusted = 'added';
-    }
-    const targets = [primary, ...additional];
-    targets.primaryTargetAdjustHint = adjusted;
-    return targets;
-  },
-
-});
 
 
 module.exports = EX;
