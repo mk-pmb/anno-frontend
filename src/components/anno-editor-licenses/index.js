38df3f4acc072fcd8657780c5f2c687ae53c9b04
mk-pmb <mk-pmb@users.noreply.github.com>
2023-02-02 20:53:37 +0100

    License chooser fires twice. Add observation and ignore.

--- src/components/anno-editor-licenses/index.js
+++ src/components/anno-editor-licenses/index.js
@@ -18,4 +18,15 @@ module.exports = {
     installPopOvers(this.$el);
   },
 
+  methods: {
+
+    setNewLicense(licUrl) {
+      // window.alert('License: ' + licUrl);
+      // 2023-01-18: For reason unknown, a single click fires this handler
+      //    twice. Ignoring due to low impact.
+      this.$store.commit('SET_EDITOR_ANNO_PROP', ['rights', licUrl]);
+    },
+
+  },
+
 };
