51b83396c77f724d59c94ec6ff5f5e3559a1cfa7
mk-pmb <mk-pmb@users.noreply.github.com>
2023-02-07 08:47:04 +0100

    Add fixtures for testing nested replies.

---
+++ test/fixtures/toller_baum.mjs
@@ -0,0 +1,183 @@
+export default [
+  { "@context": "http://www.w3.org/ns/anno.jsonld",
+    "body": { "type": "TextualBody", "format": "text/html",
+      "value": "<p>Welche Art ist das?</p>" },
+    "created": "2023-02-08T07:55:00Z",
+    "creator": { "name": "K. Lee" },
+    "dc:isVersionOf": "http://localhost:33321/anno/97482208-309e-40fd-abff-411cc25c79c3",
+    "dc:title": "Ein toller Baum.",
+    "id": "http://localhost:33321/anno/97482208-309e-40fd-abff-411cc25c79c3~1",
+    "target": [
+      {
+        "scope": "http://anno.test/displayAnnotations",
+        "selector": { "type": "SvgSelector", "value": "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\"\n  width=\"986\">\n  <rect x=\"277\" y=\"436\" width=\"94\" height=\"226\" />\n</svg>" },
+        "source": "https://upload.wikimedia.org/wikipedia/commons/d/da/Universitaet_Heidelberg_%28Karl_Lange%29_1896.jpg"
+      }
+    ]
+  },
+  {
+    "@context": "http://www.w3.org/ns/anno.jsonld",
+    "body": { "type": "TextualBody", "format": "text/html",
+      "value": "<p>Schade.</p>" },
+    "created": "2023-02-08T07:55:31Z",
+    "creator": { "name": "Prof. Eich" },
+    "dc:isVersionOf": "http://localhost:33321/anno/59488ee5-d1ab-4785-b497-42f94024ecf8",
+    "dc:title": "Jedenfalls keine Eiche.",
+    "id": "http://localhost:33321/anno/59488ee5-d1ab-4785-b497-42f94024ecf8~1",
+    "motivation": ["replying"],
+    "target": [
+      {
+        "id": "http://localhost:33321/anno/97482208-309e-40fd-abff-411cc25c79c3",
+        "scope": "http://anno.test/displayAnnotations"
+      },
+      {
+        "id": "http://anno.test/displayAnnotations"
+      }
+    ]
+  },
+  {
+    "@context": "http://www.w3.org/ns/anno.jsonld",
+    "body": { "type": "TextualBody", "format": "text/html",
+      "value": "<p>Wäre auch regional untypisch.</p>" },
+    "created": "2023-02-08T07:56:35Z",
+    "creator": { "name": "Prof. Kukui" },
+    "dc:isVersionOf": "http://localhost:33321/anno/2a44e2dc-6255-4a1c-b7a4-0cb18fc74455",
+    "dc:title": "Auch kein Lichtnussbaum.",
+    "id": "http://localhost:33321/anno/2a44e2dc-6255-4a1c-b7a4-0cb18fc74455~1",
+    "motivation": ["replying"],
+    "target": [
+      {
+        "id": "http://localhost:33321/anno/59488ee5-d1ab-4785-b497-42f94024ecf8",
+        "scope": "http://anno.test/displayAnnotations"
+      },
+      {
+        "id": "http://anno.test/displayAnnotations"
+      }
+    ]
+  },
+  {
+    "@context": "http://www.w3.org/ns/anno.jsonld",
+    "body": { "type": "TextualBody", "format": "text/html",
+      "value": "<p>Weiß zwar auch nicht welche Art, aber definitiv toller.</p>" },
+    "created": "2023-02-07T06:01:43Z",
+    "creator": { "name": "B. Brot" },
+    "dc:isVersionOf": "http://localhost:33321/anno/999a653d-211b-4ad9-92f0-d24e7741d2e7",
+    "dc:title": "Der kleine Doppelbaum ist viel toller.",
+    "id": "http://localhost:33321/anno/999a653d-211b-4ad9-92f0-d24e7741d2e7~1",
+    "motivation": ["replying"],
+    "target": [
+      {
+        "id": "http://localhost:33321/anno/97482208-309e-40fd-abff-411cc25c79c3",
+        "scope": "http://anno.test/displayAnnotations"
+      },
+      {
+        "scope": "http://anno.test/displayAnnotations",
+        "selector": {
+          "type": "SvgSelector",
+          "value": "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\"\n  width=\"986\">\n  <rect x=\"158.58166666666668\" y=\"523.0205050150554\" width=\"96.95666666666665\" height=\"144.61333333333334\" />\n</svg>"
+        },
+        "source": "https://upload.wikimedia.org/wikipedia/commons/d/da/Universitaet_Heidelberg_%28Karl_Lange%29_1896.jpg"
+      }
+    ]
+  },
+  {
+    "@context": "http://www.w3.org/ns/anno.jsonld",
+    "body": { "type": "TextualBody", "format": "text/html",
+      "value": "<p><strong class=\"ql-size-huge\">Bäume sind sowieso nur eine Erfindung der Regierung! Wacht auf, ihr Schlafschafe!</strong></p>" },
+    "created": "2023-02-07T06:03:26Z",
+    "creator": { "name": "W. Ecker" },
+    "dc:isVersionOf": "http://localhost:33321/anno/c1f7bb1b-cb04-4631-a9e8-cfb87640aede",
+    "dc:title": "Ist kein Baum!",
+    "id": "http://localhost:33321/anno/c1f7bb1b-cb04-4631-a9e8-cfb87640aede~1",
+    "motivation": ["replying"],
+    "target": [
+      {
+        "id": "http://localhost:33321/anno/999a653d-211b-4ad9-92f0-d24e7741d2e7",
+        "scope": "http://anno.test/displayAnnotations"
+      },
+      {
+        "id": "http://anno.test/displayAnnotations"
+      }
+    ]
+  },
+  {
+    "@context": "http://www.w3.org/ns/anno.jsonld",
+    "body": { "type": "TextualBody", "format": "text/html",
+      "value": "<p>Scheint mir jedenfalls nicht anerkannter Konsens zu sein. Insofern belegwürdig.</p>" },
+    "created": "2023-02-07T06:05:06Z",
+    "creator": { "name": "Nero" },
+    "dc:isVersionOf": "http://localhost:33321/anno/9e041761-e1df-475d-9784-a9ad2b59a20c",
+    "dc:title": "Quelle? Belege?",
+    "id": "http://localhost:33321/anno/9e041761-e1df-475d-9784-a9ad2b59a20c~1",
+    "motivation": ["replying"],
+    "target": [
+      {
+        "id": "http://localhost:33321/anno/c1f7bb1b-cb04-4631-a9e8-cfb87640aede",
+        "scope": "http://anno.test/displayAnnotations"
+      },
+      {
+        "id": "http://anno.test/displayAnnotations"
+      }
+    ]
+  },
+  {
+    "@context": "http://www.w3.org/ns/anno.jsonld",
+    "body": { "type": "TextualBody", "format": "text/html",
+      "value": "<p>Du hast die Anf&uuml;hrungszeichen um &quot;&quot;&quot;Regierung&quot;&quot;&quot; vergessen.</p>" },
+    "created": "2023-02-07T06:05:22Z",
+    "creator": { "name": "W. Acher" },
+    "dc:isVersionOf": "http://localhost:33321/anno/b89d9396-c335-4886-8e80-a2785d0a3cc2",
+    "dc:title": "Aw: Ist kein Baum!",
+    "id": "http://localhost:33321/anno/b89d9396-c335-4886-8e80-a2785d0a3cc2~1",
+    "motivation": ["replying"],
+    "target": [
+      {
+        "id": "http://localhost:33321/anno/c1f7bb1b-cb04-4631-a9e8-cfb87640aede",
+        "scope": "http://anno.test/displayAnnotations"
+      },
+      {
+        "id": "http://anno.test/displayAnnotations"
+      }
+    ]
+  },
+  {
+    "@context": "http://www.w3.org/ns/anno.jsonld",
+    "body": { "type": "TextualBody", "format": "text/html",
+      "value": "<p>Lies einfach ne Weile auf Twitter mit, jeder weiß es.</p>" },
+    "created": "2023-02-07T06:05:47Z",
+    "creator": { "name": "W. Ecker" },
+    "dc:isVersionOf": "http://localhost:33321/anno/624c5267-53a6-45c3-81f2-d3f435ab7845",
+    "dc:title": "Einfach mal Augen auf",
+    "id": "http://localhost:33321/anno/624c5267-53a6-45c3-81f2-d3f435ab7845~1",
+    "motivation": ["replying"],
+    "target": [
+      {
+        "id": "http://localhost:33321/anno/9e041761-e1df-475d-9784-a9ad2b59a20c",
+        "scope": "http://anno.test/displayAnnotations"
+      },
+      {
+        "id": "http://anno.test/displayAnnotations"
+      }
+    ]
+  },
+  {
+    "@context": "http://www.w3.org/ns/anno.jsonld",
+    "body": { "type": "TextualBody", "format": "text/html",
+      "value": "<p>Achwas Quelle. Einfach als Spam melden und ignorieren.</p>" },
+    "created": "2023-02-07T06:07:11Z",
+    "creator": { "name": "Spiculus" },
+    "dc:isVersionOf": "http://localhost:33321/anno/2e687496-e32c-42c3-a1f4-f7256276bf5b",
+    "dc:title": "Offensichtlicher Blödsinn",
+    "id": "http://localhost:33321/anno/2e687496-e32c-42c3-a1f4-f7256276bf5b~1",
+    "motivation": ["replying"],
+    "target": [
+      {
+        "id": "http://localhost:33321/anno/9e041761-e1df-475d-9784-a9ad2b59a20c",
+        "scope": "http://anno.test/displayAnnotations"
+      },
+      {
+        "id": "http://anno.test/displayAnnotations"
+      }
+    ]
+  }
+];
