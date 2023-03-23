c6bf92dce27b207d22361a58e40e662dfeeed0f8
mk-pmb <mk-pmb@users.noreply.github.com>
2023-03-23 18:21:10 +0100

    Universal Poster: Support custom HTTP verb

--- test/html/universalPoster.main.js
+++ test/html/universalPoster.main.js
@@ -9,16 +9,18 @@ window.jQuery().ready(function installLate() {
   const { annoEndpoint } = cfg;
   const annoEndpointAbs = cfg.resolveURL(annoEndpoint);
 
-  const panel = testUtil.addTestsPanel('Custom POST');
+  const panel = testUtil.addTestsPanel('Custom Request');
   const tpl = testUtil.makeSlotTplFuncs({
-    txf: '<p>¤: <input type="text" name="¤" size="30" value="¤">¤</p>',
+    txf: '<input type="text" name="¤" size="¤" value="¤">',
     txa: ('<p>¤:<br><textarea name="¤" cols="80" rows="10" wrap="off"'
       + ' class="code"></textarea></p>'),
   });
   panel.addForm(`
-    ${tpl.txf('Endpoint sub URL', 'subUrl', 'anno/')}
-    ${tpl.txf('Content-Type', 'cType', 'application/json; charset=UTF-8',
-      testUtil.makeCkbLabel('auto-fix JSON'))}
+    <p>Method: ${tpl.txf('verb', 8, 'POST')}
+      Endpoint: ${tpl.txf('subUrl', 30, 'anno/')}</p>
+    <p>Content-Type: ${tpl.txf('cType', 30,
+      'application/json; charset=UTF-8')
+      } ${testUtil.makeCkbLabel('auto-fix JSON')}</p>
     ${tpl.txa('Raw body text to be sent', 'bodyTxa')}
     ${tpl.txa("Server's response", 'rspTxa')}
   `, function setup(form) {
@@ -29,6 +31,17 @@ window.jQuery().ready(function installLate() {
       rspTxa.value = '[' + testUtil.localTimeHr() + '] ' + m;
     }
 
+    bodyTxa.onblur = function fx() {
+      const tx = bodyTxa.value;
+      if (!tx.startsWith('//!= {')) { return; }
+      let ovr = tx.split(/\n/)[0];
+      const cut = ovr.length + 1;
+      ovr = ovr.trim().replace(/^\S+/, '');
+      try { ovr = JSON.parse(ovr); } catch (e) { return !e; }
+      Object.keys(ovr).forEach(k => { form.elements[k].value = ovr[k]; });
+      bodyTxa.value = tx.slice(cut);
+    };
+
     async function submit() {
       let data = bodyTxa.value;
       const mainCType = cType.value.split(/;/)[0];
@@ -38,7 +51,7 @@ window.jQuery().ready(function installLate() {
         data = data.replace(/,(?=\s*\})/g, '');
       }
       const request = {
-        method: 'post',
+        method: form.elements.verb.value,
         url: annoEndpointAbs + form.elements.subUrl.value,
         contentType: cType.value,
         data,
