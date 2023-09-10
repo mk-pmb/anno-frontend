95a21fc4f2eca582aeabe98e55faa86a7069b682
mk-pmb <mk-pmb@users.noreply.github.com>
2023-09-10 20:10:27 +0200

    Mixin annoUrls: Prepare refactor: Indent.

--- src/mixin/annoUrls.js
+++ src/mixin/annoUrls.js
@@ -3,34 +3,34 @@
 const isStr = require('is-string');
 
 
-function annoIdToPermaUrl(annoIdOrVersId) {
-  /*
-    The "perma" here is meant to be the canonical URL that people can
-    bookmark and share. Ideally it should tend to be short and also be
-    somewhat permanent, even if less strictly than a DOI would be.
-  */
-  if (!annoIdOrVersId) { return ''; }
-  const { state } = this.$store;
-  let url = state.purlTemplate;
-  if (!url) { return '#!missingConfig:purlTemplate!' + annoIdOrVersId; }
-  const versionIdentifier = annoIdOrVersId.split('/').slice(-1)[0];
+  function annoIdToPermaUrl(annoIdOrVersId) {
+    /*
+      The "perma" here is meant to be the canonical URL that people can
+      bookmark and share. Ideally it should tend to be short and also be
+      somewhat permanent, even if less strictly than a DOI would be.
+    */
+    if (!annoIdOrVersId) { return ''; }
+    const { state } = this.$store;
+    let url = state.purlTemplate;
+    if (!url) { return '#!missingConfig:purlTemplate!' + annoIdOrVersId; }
+    const versionIdentifier = annoIdOrVersId.split('/').slice(-1)[0];
 
-  // %sl and {{ slug }} are deprecated and will be dropped soon!
-  url = url.replace('%sl', versionIdentifier);
-  url = url.replace('{{ slug }}', versionIdentifier);
+    // %sl and {{ slug }} are deprecated and will be dropped soon!
+    url = url.replace('%sl', versionIdentifier);
+    url = url.replace('{{ slug }}', versionIdentifier);
 
-  url = url.replace('%ep', state.annoEndpoint);
-  url = url.replace('%ri', versionIdentifier);
+    url = url.replace('%ep', state.annoEndpoint);
+    url = url.replace('%ri', versionIdentifier);
 
-  return url;
-}
+    return url;
+  }
 
 
-function findResourceUrl(res) {
-  if (!res) { return ''; }
-  if (isStr(res)) { return res; }
-  return (res.id || res.source || '');
-}
+  function findResourceUrl(res) {
+    if (!res) { return ''; }
+    if (isStr(res)) { return res; }
+    return (res.id || res.source || '');
+  }
 
 
 
