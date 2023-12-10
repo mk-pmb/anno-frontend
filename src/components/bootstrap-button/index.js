2dbb5075e325ba6e312e16928ea5446da9f01235
mk-pmb <mk-pmb@users.noreply.github.com>
2023-12-10 23:04:52 +0100

    Add unpublish button.

--- src/components/bootstrap-button/index.js
+++ src/components/bootstrap-button/index.js
@@ -21,6 +21,10 @@ module.exports = {
     require('./balloon.scss'),
   ],
 
+  mixins: [
+    require('../../mixin/l10n.js'),
+  ],
+
   props: {
     caption:      String,
     title:        String,
@@ -39,6 +43,7 @@ module.exports = {
     elem:         { type: String },
     balloonColorName: { type: String, default: 'secondary' },
     popoverContentOpts: { type: [Object] },
+    irrevocableConfirm: Boolean,
   },
 
   mounted() {
@@ -51,6 +56,7 @@ module.exports = {
         ...btn.popoverContentOpts,
       });
     }
+    btn.$el.getVueElem = () => btn;
   },
 
   methods: {
@@ -79,6 +85,11 @@ module.exports = {
 
     clicked(ev) {
       const btn = this;
+      if (btn.irrevocableConfirm) {
+        const q = (btn.l10n('confirm_irrevocable_action') + '\n'
+          + ((btn.$el.innerText || '').trim() || btn.l10n('no_data')));
+        if (!window.confirm(q)) { return false; }
+      }
       btn.$emit('click', ev);
       if (btn.eventbusEmit) { eventBus.$emit(btn.eventbusEmit); }
       if (btn.$slots.balloon) { btn.$el.classList.toggle('balloon-open'); }
