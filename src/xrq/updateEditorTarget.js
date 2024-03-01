bcdfec1f8eda3bc0d42a0c7e5b34030acbf866c5
mk-pmb <mk-pmb@users.noreply.github.com>
2024-03-01 17:26:13 +0100

    Add stub for externalRequest action UpdateEditorTarget.

---
+++ src/xrq/updateEditorTarget.js
@@ -0,0 +1,16 @@
+﻿/* -*- tab-width: 2 -*- */
+'use strict';
+
+function orf(x) { return x || false; }
+
+
+async function update(store, origParam) {
+  const { fragment, selector } = orf(origParam);
+  console.debug('Anno-Frontend: Received editor target update:',
+    { fragment, selector }, 'Selector value:', orf(selector).value);
+}
+
+
+module.exports = {
+  doUpdateEditorTarget: update,
+};
