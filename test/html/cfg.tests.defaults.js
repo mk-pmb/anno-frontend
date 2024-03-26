1935e2fd74d28555bb7a8214bb59af2f8d3abd26
mk-pmb <mk-pmb@users.noreply.github.com>
2024-03-26 17:36:58 +0100

    [test][docs] When and why to fix target on version selection change.

--- test/html/cfg.tests.defaults.js
+++ test/html/cfg.tests.defaults.js
@@ -23,8 +23,36 @@
 
   cfg.initCmpOnVersionSelected = function onVersionSelected(event) {
     console.debug('Version selected:', event);
+
+    /* For debugging, it may be helpful to export the event so you can
+      more easily inspect it in the browser console: */
     window.versionSelectedEvent = event;
+
+    // No UI changes required for invisible changes:
+    if (!event.sideVisible) { return; }
+
+    /* One of the first things this handler should do is to retract any
+      outdated UI components. This way, if one of the next steps has some
+      obscure bug that throws an exception, users at least will know that
+      something broke, rather than rely on outdated visual displays. */
+    // window.mainApp.clearTargetPreviews();
+
+    /* Webdesign best practice is to have the initial website view rendered
+      server-side (see "Why SSR?"[1]). With SSR, the webserver would request
+      the annotation internally and would be able to determine and configure
+      the target correctly in the initial AnnoApp config.
+      [1] https://vuejs.org/guide/scaling-up/ssr.html
+
+      However, for debugging, we'd prefer using only static HTML pages.
+      We thus start with a degraded initial config, condone a misconfigured
+      target and then fix it later.
+      The frontend has a convenience method to do this fix-up: */
     event.adjustConfiguredTargetSource();
+
+    /* In scenarios with target previews, we should now render the
+      preview components for the new targets. */
+    // const subjectTargets = evt.categorizeTargets().subjects;
+    // subjectTargets.forEach(window.mainApp.addTargetPreview);
   };
 
   // cfg.events.appReady = function ready() {};
