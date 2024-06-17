481612a5140429212461d67822b965037ad54ebd
mk-pmb <mk-pmb@users.noreply.github.com>
2024-06-17 02:54:10 +0200

    [minor] relationlink-editor: Adjust whitespace.

--- src/components/relationlink-editor/relationlink-editor.js
+++ src/components/relationlink-editor/relationlink-editor.js
@@ -11,35 +11,48 @@ const dfPredicates = require('./predi.default.js');
 
 module.exports = {
     mixins: [
-        require('../../mixin/l10n'),
-        require('./determinePredicateCaption.js'),
+      require('../../mixin/l10n'),
+      require('./determinePredicateCaption.js'),
     ],
     template: require('./relationlink-editor.html'),
     // style:    require('./bonanza.sass'),
+
     props: {
       knownPredicates: { type: Array, default: dfPredicates.allUrls.boundSlice },
     },
+
     computed: {
-        relationLinkBodies() {return this.$store.getters.relationLinkBodies}
+      relationLinkBodies() { return this.$store.getters.relationLinkBodies },
     },
+
     methods: {
-        addRelationLink() {
-          this.$store.commit('ADD_RELATIONLINK', {
-            predicate: dfPredicates.allUrls[0],
+
+      addRelationLink() {
+        this.$store.commit('ADD_RELATIONLINK', {
+          predicate: dfPredicates.allUrls[0],
+        });
+      },
+
+      removeBody(body) {this.$store.commit('REMOVE_BODY', body)},
+
+      includes(maybeArray, val) {
+        return Array.isArray(maybeArray) && maybeArray.includes(val)
+      },
+
+      onSelectPurpose(bodyIndex, event) {
+        const { value } = event.target;
+          console.debug('onSelectPurpose', { ctx: this, bodyIndex, value });
+          this.$store.commit("SET_RELATIONLINK_PROP", {
+            n: bodyIndex,
+            prop: 'predicate',
+            value,
           });
-        },
-        removeBody(body) {this.$store.commit('REMOVE_BODY', body)},
-        includes(maybeArray, val) {
-            return Array.isArray(maybeArray) && maybeArray.includes(val)
-        },
-        onSelectPurpose(bodyIndex, event) {
-          const { value } = event.target;
-            console.debug('onSelectPurpose', { ctx: this, bodyIndex, value });
-            this.$store.commit("SET_RELATIONLINK_PROP", {
-              n: bodyIndex,
-              prop: 'predicate',
-              value,
-            });
-        },
+      },
+
+      toggleVisibility(refName) {
+        const s = (this.$refs[refName] || false).style;
+        if (s) { s.display = (s.display === 'none' ? '' : 'none'); }
+      },
+
     },
 }
