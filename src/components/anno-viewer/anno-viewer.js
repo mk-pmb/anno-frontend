17551e519bac78bbc4afd22c7c57679ffe4f21a3
mk-pmb <mk-pmb@users.noreply.github.com>
2024-07-30 18:43:25 +0200

    Anno-Viewer: Define .annoData as a null-safe proxy for .annotation.

--- src/components/anno-viewer/anno-viewer.js
+++ src/components/anno-viewer/anno-viewer.js
@@ -89,7 +89,7 @@ module.exports = {
     data() {
       const el = this;
       const { state } = el.$store;
-      const anno = orf(el.annotation);
+      const anno = orf(el.annotation); // .annoData isn't available yet
       const hasRealPublicDoi = Boolean(anno['dc:identifier']);
       // cdbg('initData el.isListViewItem', [el.isListViewItem]);
 
@@ -209,14 +209,14 @@ module.exports = {
   },
 
   computed: {
-    annoIdUrl()          { return this.annotation.id || ''; },
+    annoData() { return orf(this.annotation); },
+    annoIdUrl() { return this.annoData.id || ''; },
 
     firstHtmlBody()      {return textualHtmlBody.first(this.annotation)},
     svgTarget()          {return svgSelectorResource.first(this.annotation)},
 
     title() {
-      const anno = this.annotation;
-      if (!anno) { return ''; }
+      const anno = this.annoData;
       return String(anno['dc:title'] || anno.title || '');
     },
 
@@ -238,7 +238,7 @@ module.exports = {
 
 
     approval() {
-      const val = this.annotation['dc:dateAccepted'];
+      const val = this.annoData['dc:dateAccepted'];
       const st = { val, active: true, explain: '' };
       if (val === undefined) { return st; } // i.e. no approval required
       let icon = '';
@@ -294,7 +294,7 @@ module.exports = {
 
 
     creatorsList() {
-      const { creator } = this.annotation;
+      const { creator } = this.annoData;
       if (!creator) { return []; }
       const list = jsonDeepCopy([].concat(creator).filter(Boolean));
       const lastItem = list.slice(-1);
@@ -304,7 +304,7 @@ module.exports = {
 
 
     currentLicense() {
-      const licUrl = this.annotation.rights;
+      const licUrl = this.annoData.rights;
       const licInfo = orf(licensesByUrl.get(licUrl));
       return licInfo;
     },
@@ -358,8 +358,8 @@ module.exports = {
     formatters,
     toggleDetailBar,
 
-    revise() { return eventBus.$emit('revise', this.annotation) },
-    reply()  { return eventBus.$emit('reply',  this.annotation) },
+    revise() { return eventBus.$emit('revise', this.annoData) },
+    reply()  { return eventBus.$emit('reply',  this.annoData) },
 
     async approve() {
       await simpleDateStamp(this, 'dc:dateAccepted');
@@ -476,7 +476,7 @@ module.exports = {
 
 
     replyRefNumText() {
-      const anno = this.annotation;
+      const anno = this.annoData;
       const ref = anno[':ANNO_FE:replyRefNum'];
       const tgt = anno[':ANNO_FE:inReplyToRefNum'];
       return this.l10n(tgt ? 'reply_refnum_deep' : 'reply_refnum_lv1'
@@ -484,9 +484,7 @@ module.exports = {
     },
 
 
-    otherVersionsExist() {
-      return !!this.annotation['dc:replaces'];
-    },
+    otherVersionsExist() { return !!this.annoData['dc:replaces']; },
 
 
     decideShowPurlButton() {
