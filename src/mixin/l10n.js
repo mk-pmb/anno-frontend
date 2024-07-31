eede7ec8a518d0fab4bd811189ae733f2a4ea450
mk-pmb <mk-pmb@users.noreply.github.com>
2024-08-01 01:35:39 +0200

    l10n Mixin: Drop deprecated documentation.

--- src/mixin/l10n.js
+++ src/mixin/l10n.js
@@ -39,34 +39,6 @@ function l10n(cfg, vocKey, fallback) {
 }
 
 
-/**
- * ### `this.l10n(text)`
- *
- * Localization mixin. Will return the localized string in the currently
- * enabled `language`.
- *
- * Translations are kept in [`../../l10n-config.json`](./tree/master/l10n-config.json) in an object
- *
- * ```
- * config.localizations = {
- *   de: {
- *     login: 'Anmelden',
- *   },
- *   en: {
- *     login: 'Log in',
- *   },
- * }
- * ```
- *
- * If no translation for the enabled language is available, fall back to the
- * `defaultLang`.
- *
- * If there is no translation in the `defaultLang` (which is a bug) just return
- * the string.
- *
- *
- */
-
 module.exports = {
   methods: {
     l10n(...args) { return l10n(this.$store.state, ...args); },
