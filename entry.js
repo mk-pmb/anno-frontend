c21c7342015945476c2ed331377bcfe3322713ae
mk-pmb <mk-pmb@users.noreply.github.com>
2024-08-01 03:29:47 +0200

    Restructure the entire app API. (display-annotations -> ubhdAnnoApp)

--- entry.js
+++ entry.js
@@ -1,2 +1,2 @@
 'use strict';
-window.displayAnnotations = require('./src/main.js');
+window.ubhdAnnoApp = require('./src/main.js');
--- test/html/cfg.tests.target-examples.js
+++ test/html/cfg.tests.target-examples.js
@@ -2,40 +2,46 @@
 /* -*- tab-width: 2 -*- */
 (function prepareTargetExamples() {
   'use strict';
-  var cfg = window.annoTestCfg, st = {};
-  cfg.setTarget = st;
+  var cfg = window.ubhdAnnoApp.configure, st = {};
+  window.annoTestSetTarget = st;
   function stDef(f) { st[f.name] = f; }
 
   stDef(function wikiCommons(w, h, imgSubUrl) {
     var baseUrl = 'https://upload.wikimedia.org/wikipedia/commons/';
-    cfg.targetImage = baseUrl + imgSubUrl;
-    cfg.targetThumbnail = baseUrl + 'thumb/' + imgSubUrl + '/200px-_.png';
-    cfg.targetImageWidth = w;
-    cfg.targetImageHeight = h;
+    cfg({
+      targetImage: baseUrl + imgSubUrl,
+      targetThumbnail: baseUrl + 'thumb/' + imgSubUrl + '/200px-_.png',
+      targetImageWidth: w,
+      targetImageHeight: h,
+    });
   });
 
   stDef(function uniHdKarlLange1896() {
-    cfg.setTarget.wikiCommons(986, 732,
+    st.wikiCommons(986, 732,
       'd/da/Universitaet_Heidelberg_%28Karl_Lange%29_1896.jpg');
   });
 
   st.ubHdDigLitBaseUrl = 'https://digi.ub.uni-heidelberg.de/diglit/';
 
   stDef(function ubHdDigLit(w, h, subUrl, origFileName, baseUrl) {
-    var src = (baseUrl || cfg.setTarget.ubHdDigLitBaseUrl) + subUrl;
-    cfg.targetSource = src;
-    cfg.targetImage = src + '/_image';
-    cfg.targetImageWidth = w;
-    cfg.targetImageHeight = h;
-    cfg.targetThumbnail = src + '/_thumb_image';
-    cfg.iiifUrlTemplate = ('https://digi.ub.uni-heidelberg.de/iiif/2/'
+    var src = (baseUrl || st.ubHdDigLitBaseUrl) + subUrl, t;
+    t = ('https://digi.ub.uni-heidelberg.de/iiif/2/'
       + subUrl.replace(/\/\w+$/, '') + ':' + origFileName
       + '/%ir/full/0/default.jpg');
+    cfg({
+      targetSource: src,
+      targetImage: src + '/_image',
+      targetImageWidth: w,
+      targetImageHeight: h,
+      targetThumbnail: src + '/_thumb_image',
+      iiifUrlTemplate: t,
+    });
   });
 
   stDef(function ubHdJournal(zsKuerzel, submissionId) {
-    cfg.targetSource = ('https://journals.ub.uni-heidelberg.de/index.php/'
-      + zsKuerzel + '/article/view/' + submissionId);
+    var t = 'https://journals.ub.uni-heidelberg.de/index.php/'
+      + zsKuerzel + '/article/view/' + submissionId;
+    cfg({ targetSource: t });
   });
 
 }());
