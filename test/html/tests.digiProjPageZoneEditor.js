06bb57ba6253ed06ea544d03fdb14c96a0012c91
mk-pmb <mk-pmb@users.noreply.github.com>
2024-08-01 02:04:06 +0200

    [test] Example for how to edit zones in heiImageViewer.

---
+++ test/html/tests.digiProjPageZoneEditor.js
@@ -0,0 +1,44 @@
+(function setupDigiZoneEditor() {
+  'use strict';
+  const annoAppLurkReason = 'externalTargetEditor';
+  let digiProjPage = false;
+
+  window.jQuery().ready(function getDigiRef() {
+    digiProjPage = ((window.heiImageViewer
+      && (window._digiCurrentView === 'VIEW_PROJECT_PAGE')
+      && window.DigiProjectPage
+      && (window.app instanceof window.DigiProjectPage)
+      && window.app) || false);
+  });
+
+  function doneEditing() {
+    window.ubhdAnnoApp.abortLurkMode(annoAppLurkReason);
+  }
+
+  function startExternalTargetEditing(event) {
+    const target = event.annoTarget;
+    if (!target) { throw new Error('No target to edit!'); }
+    if (!digiProjPage) { throw new Error('Not running in DigiProjectPage!'); }
+    if (!event.replaceExistingContent) {
+      throw new Error('Expected option replaceExistingContent!');
+    }
+    digiProjPage.startSvgZoneEditing({
+      domContainer: event.domContainer,
+      initialSvg: ((target.selector || false).value || ''),
+      onDone: doneEditing,
+      onUpdated(newSvg) {
+        let selector = null;
+        if (newSvg) { selector = { type: 'SvgSelector', value: newSvg }; }
+        window.ubhdAnnoApp.externalRequest('UpdateEditorTarget', { selector });
+      },
+    });
+  }
+
+
+  window.ubhdAnnoApp.configure({
+    events: {
+      startExternalTargetEditing,
+      abortExternalTargetEditing() { digiProjPage.stopSvgZoneEditing(); },
+    },
+  });
+}());
