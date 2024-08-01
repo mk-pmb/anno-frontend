cf44c3db5c1f048d00f5e0b22c52e5614193d2cc
mk-pmb <mk-pmb@users.noreply.github.com>
2024-08-01 04:30:25 +0200

    Implement lurk mode.

--- src/xrq/updateEditorTarget.js
+++ src/xrq/updateEditorTarget.js
@@ -5,9 +5,16 @@ function orf(x) { return x || false; }
 
 
 async function update(store, origParam) {
+  const { editor } = this.annoApp;
   const { fragment, selector } = orf(origParam);
+  const { type: selType, value: selVal, ...selOther } = orf(selector);
   console.debug('Anno-Frontend: Received editor target update:',
-    { fragment, selector }, 'Selector value:', orf(selector).value);
+    { fragment, selType, selVal, selOther });
+  if (!selector) {
+    editor.setZoneSelector('');
+    return;
+  }
+  if (selType === 'SvgSelector') { editor.setZoneSelector(selVal); }
 }
 
 
