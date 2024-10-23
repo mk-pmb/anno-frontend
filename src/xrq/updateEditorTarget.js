b406f9b6b1de489b91a20adaf173f1e0675abbcd
mk-pmb <mk-pmb@users.noreply.github.com>
2024-10-23 21:49:44 +0200

    Add option debugConfirmXrqEraseSelector.

--- src/xrq/updateEditorTarget.js
+++ src/xrq/updateEditorTarget.js
@@ -11,6 +11,12 @@ async function update(store, origParam) {
   console.debug('Anno-Frontend: Received editor target update:',
     { fragment, selType, selVal, selOther });
   if (!selector) {
+    const k = 'debugConfirmXrqEraseSelector';
+    if (store.state[k]) {
+      const msg = (k + '?\n' + (new Error(k + '?')).stack
+        ).replace(/\n/g, '\n\n');
+      if (!window.confirm(msg)) { return; }
+    }
     editor.setZoneSelector('');
     return;
   }
