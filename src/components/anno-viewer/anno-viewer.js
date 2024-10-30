b33ce6206e2ccbce8dd4e50871b374cbaaae4684
mk-pmb <mk-pmb@users.noreply.github.com>
2024-10-30 21:35:21 +0100

    Add option to open the additional targets hint link in a new window.

--- src/components/anno-viewer/anno-viewer.js
+++ src/components/anno-viewer/anno-viewer.js
@@ -172,6 +172,7 @@ module.exports = {
         introText,
         linkText: '[' + el.l10n('additional_subjects_show') + ']',
         linkUrl: assembleVersionRelatedUrl(state, 'versionsButton', anno),
+        linkFrame: state.additionalTargetsHintLinkFrame || '',
       });
     }());
 
