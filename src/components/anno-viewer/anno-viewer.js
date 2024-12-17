57ca9153e46b010a7a25f19147fc909f6216f2dc
mk-pmb <mk-pmb@users.noreply.github.com>
2024-12-17 22:02:57 +0100

    Anno-Viewer: creatorsList: Fix lastItem condition.

--- src/components/anno-viewer/anno-viewer.js
+++ src/components/anno-viewer/anno-viewer.js
@@ -282,7 +282,7 @@ module.exports = {
       const { creator } = this.annoData;
       if (!creator) { return []; }
       const list = jsonDeepCopy([].concat(creator).filter(Boolean));
-      const lastItem = list.slice(-1);
+      const [lastItem] = list.slice(-1);
       if (lastItem) { lastItem['x-is-last-in-list'] = true; }
       return list;
     },
