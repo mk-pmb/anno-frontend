e1d33e6ba679b7a5911d4ad5d31a663dab291236
mk-pmb <mk-pmb@users.noreply.github.com>
2025-02-22 21:38:54 +0100

    EventBus: Support multiEmit.

--- src/event-bus.js
+++ src/event-bus.js
@@ -3,9 +3,25 @@ const Vue = require('vue').default;
 
 const bus = new Vue();
 
+function fail(why) { throw new Error(why); }
+
 Object.assign(bus, {
 
   // simpleEmitter: memoize(evName => () => bus.$emit(evName)),
+
+  multiEmit(queue, ...unsupported) {
+    if (!Array.isArray(queue)) { fail('Event queue must be an array.'); }
+    if (unsupported.length) { fail('Too many arguments.'); }
+    let nextIndex = 0;
+    // console.debug('eventBus multiEmit:', queue);
+    (function emitNext() {
+      const ev = queue[nextIndex];
+      // console.debug('eventBus multiEmit: emitNext:', nextIndex, ev);
+      nextIndex += 1;
+      if (ev) { bus.$emit(...[].concat(ev)); }
+      if (nextIndex < queue.length) { setTimeout(emitNext, 100); }
+    }());
+  },
 
 });
 
