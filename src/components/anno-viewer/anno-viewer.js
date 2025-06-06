3d7d26fe84f88626b76e2f9a8d7a6e6fb31efe68
mk-pmb <mk-pmb@users.noreply.github.com>
2025-06-06 13:53:29 +0200

    Fix bootstrap: Class pull-(left|right) is called float-… in BS4.

--- src/components/anno-viewer/anno-viewer.js
+++ src/components/anno-viewer/anno-viewer.js
@@ -139,7 +139,7 @@ module.exports = {
       cachedIiifLink: '',
       collapsed,
       currentVersionDoiUri: String(anno['dc:identifier'] || ''),
-      detailBarClipCopyBtnCls: 'pull-right',
+      detailBarClipCopyBtnCls: 'float-right',
       doiLinkPreviewWarning: '',
       hasRealPublicDoi,
       highlighted: !!el.initiallyHighlighted,
