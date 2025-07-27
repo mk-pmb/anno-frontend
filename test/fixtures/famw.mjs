c27160309ee688ae4f158f795bfc5fed64d53f92
mk-pmb <mk-pmb@users.noreply.github.com>
2025-07-27 11:21:19 +0200

    [test] Add some more fixtures, including from German Sales.

---
+++ test/fixtures/famw.mjs
@@ -0,0 +1,44 @@
+export default [
+
+{ "@context": "http://www.w3.org/ns/anno.jsonld",
+  "body": [
+    { "format": "text/html",
+      "type": "TextualBody",
+      "value": "<p>Einliefernde Institution war das Finanzamt Moabit-West, Berlin. Das Versteigerungsgut, das einen Erlös von…</p>"
+    },
+    { "dc:title": "Lange, Hans W., Auktionshaus, Berlin",
+      "purpose": "classifying",
+      "source": "https://d-nb.info/gnd/16139635-5",
+      "type": "SpecificResource"
+    },
+    { "dc:title": "Finanzamt Moabit-West, Berlin (Einlieferung „F. M-W., Berlin“), Auktion Hans W. Lange 16.12.1941",
+      "purpose": "linking",
+      "rdf:predicate": "http://www.w3.org/2000/01/rdf-schema#seeAlso",
+      "source": "https://doi.org/10.11588/anno.diglit.XrK9PNSHQkK3Q0-gKwkJ6g",
+      "type": "SpecificResource"
+    }
+  ],
+  "created": "2024-12-18T14:13:25Z",
+  "creator": {
+    "id": "urn:uuid:e66e4f3b-aa06-5bba-957d-b21e17ed215e",
+    "name": "Hanna Strzoda",
+    "type": "Person"
+  },
+  "dc:identifier": "https://doi.org/10.11588/anno.diglit.o2ang_gmtd2wkqumhbeicq_8",
+  "dc:language": "de-DE",
+  "dc:title": "Finanzamt Moabit-West, Berlin (Einlieferung „F.A.M.-W., Berlin“), Auktion Hans W. Lange 12.-13.5.1942",
+  "id": "O2ANg_gMTd2wkQumHBEIcQ~8",
+  "ubhd:anno-user": "hanna",
+  "rights": "https://creativecommons.org/licenses/by/4.0/",
+  "target": {
+    "scope": "https://digi.ub.uni-heidelberg.de/diglit/lange1942_05_12/0005",
+    "selector": {
+      "type": "SvgSelector",
+      "value": "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\"\n  width=\"1465\">\n  <polygon points=\"634,1099 737,1099 739,1098 739,1192 547,1194 547,1224 173,1225 171,1133 634,1129\" />\n</svg>"
+    },
+    "source": "https://digi.ub.uni-heidelberg.de/diglit/lange1942_05_12/0005/_image"
+  },
+  "type": "Annotation"
+},
+
+];
---
+++ test/fixtures/kdmkk.mjs
@@ -0,0 +1,61 @@
+export default [
+
+{ "@context": "http://www.w3.org/ns/anno.jsonld",
+  "body": {
+    "format": "text/html",
+    "type": "TextualBody",
+    "value": "<p>schaut auf die Stadt.</p>"
+  },
+  "created": "2025-07-24T19:31:00Z",
+  "creator": {
+    "id": "urn:uuid:f85219f1-c280-5c6f-9099-31e4d4ec645f",
+    "name": "A. Nym",
+    "type": "Person"
+  },
+  "dc:language": "de-DE",
+  "dc:title": "Brückenstatue",
+  "id": "test-kdmkk-brueckenstatue~1",
+  "rights": "https://creativecommons.org/publicdomain/zero/1.0/",
+  "ubhd:anno-user": "arno",
+  "target": {
+    "dc:title": "📄26 📕 Kunstdenkmäler des Kreises Konstanz (Band 1)",
+    "selector": {
+      "type": "SvgSelector",
+      "value": "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"1611\">\n<rect x='1045' y='1380' width='55' height='110'/></svg>"
+    },
+    "scope": "https://digi.ub.uni-heidelberg.de/diglit/kraus1924/0026",
+    "source": "https://digi-test.ub.uni-heidelberg.de/diglit/kraus1924/0026/_image"
+  },
+  "type": "Annotation"
+},
+
+{ "@context": "http://www.w3.org/ns/anno.jsonld",
+  "body": {
+    "format": "text/html",
+    "type": "TextualBody",
+    "value": "<p>wächst von der Stadt weg, als wolle er fliehen.</p>"
+  },
+  "created": "2025-07-24T19:32:00Z",
+  "creator": {
+    "id": "urn:uuid:f85219f1-c280-5c6f-9099-31e4d4ec645f",
+    "name": "A. Nym",
+    "type": "Person"
+  },
+  "dc:language": "de-DE",
+  "dc:title": "Baum hinter Brücke",
+  "id": "test-kdmkk-baum-hinter-bruecke~1",
+  "rights": "https://creativecommons.org/publicdomain/zero/1.0/",
+  "ubhd:anno-user": "arno",
+  "target": {
+    "dc:title": "📄26 📕 Kunstdenkmäler des Kreises Konstanz (Band 1)",
+    "selector": {
+      "type": "SvgSelector",
+      "value": "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"1611\">\n<rect x='1120' y='1300' width='165' height='170'/></svg>"
+    },
+    "scope": "https://digi.ub.uni-heidelberg.de/diglit/kraus1924/0026",
+    "source": "https://digi-test.ub.uni-heidelberg.de/diglit/kraus1924/0026/_image"
+  },
+  "type": "Annotation"
+},
+
+];
