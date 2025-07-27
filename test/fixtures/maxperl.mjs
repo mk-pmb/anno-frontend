1cf926e8cc0dff726c31fcdc3ff5188d7880cf7a
mk-pmb <mk-pmb@users.noreply.github.com>
2025-07-27 11:21:19 +0200

    [test] Add some more fixtures, including from German Sales.

---
+++ test/fixtures/maxperl.mjs
@@ -0,0 +1,32 @@
+export default [
+
+{ "@context": "http://www.w3.org/ns/anno.jsonld",
+  "body": {
+    "format": "text/html",
+    "type": "TextualBody",
+    "value": "<p>zeigt die Windrichtung.</p>"
+  },
+  "created": "2025-07-24T19:41:00Z",
+  "creator": {
+    "id": "urn:uuid:f85219f1-c280-5c6f-9099-31e4d4ec645f",
+    "name": "A. Nym",
+    "type": "Person"
+  },
+  "dc:language": "de-DE",
+  "dc:title": "Wetterhahn auf Turm",
+  "id": "test-maxperl-wetterhahn~1",
+  "rights": "https://creativecommons.org/publicdomain/zero/1.0/",
+  "ubhd:anno-user": "arno",
+  "target": {
+    "dc:title": "📄34 📕 Max Perl <Berlin> [Editor]: Deutsche Literatur, illu…",
+    "scope": "https://digi.ub.uni-heidelberg.de/diglit/perl1937_03_18/0036",
+    "selector": {
+      "type": "SvgSelector",
+      "value": "\n<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"1765\">\n<circle cx='706' cy='734' r='77'/></svg>"
+    },
+    "source": "https://digi-test.ub.uni-heidelberg.de/diglit/perl1937_03_18/0036/_image"
+  },
+  "type": "Annotation"
+},
+
+];
