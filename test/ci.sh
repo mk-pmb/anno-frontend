4897740a6718a40c2f9c5d5f244b438a810caf84
mk-pmb <mk-pmb@users.noreply.github.com>
2025-10-23 19:02:32 +0200

    [ci] Set up auto-release.

--- test/ci.sh
+++ test/ci.sh
@@ -10,6 +10,13 @@ function ci_test () {
 
   npm install --global npm@7 || return $?
   npm install . || return $?
+
+  ### BEGIN ### Hotfix for vue2-teleport issue 13 (2025-10-23) ###
+  sed -re 's~( map: \{"version":3,"sources":\["/)[A-Za-z/]+'$(
+    )'(/vue2-teleport/src/)~\1…\2~g' -i \
+    -- node_modules/vue2-teleport/dist/teleport.*
+  ### ENDOF ### Hotfix for vue2-teleport issue 13 (2025-10-23) ###
+
   npm run build || return $?
   npm test || return $?
 }
