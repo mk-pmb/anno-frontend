f257a1090b06cbc1de8ac31c3c48e625553b24d3
mk-pmb <mk-pmb@users.noreply.github.com>
2025-11-16 17:32:49 +0100

    Implement ubhdAnnoApp.getCurrentAnnosList().

--- src/main.js
+++ src/main.js
@@ -54,6 +54,7 @@ const eventBusProxies = objFromKeysList(function makeEventBusProxy(ev) {
 
 let vueRootElem;
 let configAccum = decideDefaultOptions();
+let currentAnnosList = false;
 
 const EX = {
 
@@ -205,6 +206,10 @@ const EX = {
   },
 
 
+  getCurrentAnnosList() { return currentAnnosList; }, /*
+    No protections required: It's stored from the eventBus event,
+    so it should already be a frozen array. */
+
 };
 
 
@@ -233,6 +238,7 @@ EX.reuseLib = (function compile() {
 
 
 
+eventBus.$on('annoListFetchedRaw', (list) => { currentAnnosList = list; });
 
 
 eventBus.$once('annoListReplaced',
