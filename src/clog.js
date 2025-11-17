4f036d7d44c2a9968bdcdc34b068c41a846f32f0
mk-pmb <mk-pmb@users.noreply.github.com>
2025-11-17 06:30:50 +0100

    Anno-Viewer: Factor out the prefix logger.

---
+++ src/clog.js
@@ -0,0 +1,13 @@
+﻿// clog etc. = console.log (and similar), but with a prefix for easier trace.
+
+const EX = function loggerFactory(prefix) {
+  const f = m => console[m].bind(console, prefix + ':');
+  const l = {
+    cdbg: f('debug'),
+    cerr: f('error'),
+    cwarn: f('warn'),
+  };
+  return l;
+};
+
+module.exports = EX;
