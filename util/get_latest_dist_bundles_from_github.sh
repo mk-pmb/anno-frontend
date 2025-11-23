12b1ea5507f6dd5938f0dac2dd3c2aacc1d20bf7
mk-pmb <mk-pmb@users.noreply.github.com>
2025-11-23 20:40:06 +0100

    [ci] Pack a ZIP with all dist bundles.

---
+++ util/get_latest_dist_bundles_from_github.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# -*- coding: utf-8, tab-width: 2 -*-
+set -e
+cd -- "$(readlink -m -- "$BASH_SOURCE"/../..)"
+exec </dev/null
+
+RLS="$1"; shift || true
+case "$RLS" in
+  e ) RLS='ci:experimental';;
+  g | '' ) RLS='ci:staging';;
+  m ) RLS='ci:master';;
+  s ) RLS='ci:stable';;
+esac
+RLS="${RLS/#ci:/auto-ci-release/}"
+
+BUN='all_dist_bundles.zip'
+REPO='mk-pmb/anno-frontend'
+URL="https://github.com/$REPO/releases/download/$RLS/$BUN"
+ZIP="tmp.dl-$$.$BUN"
+wget --output-document="$ZIP".part -- "$URL"
+mv --verbose --no-target-directory -- "$ZIP"{.part,}
+( mkdir --parents dist && cd -- dist && unzip -jo ../"$ZIP" )
+rm --verbose -- "$ZIP"
