d66a7b435f56015f9af9d27cc035db18b9ef198c
mk-pmb <mk-pmb@users.noreply.github.com>
2026-05-17 16:14:20 +0200

    GitHub update helper: Wait for GitHub CI if running.

--- util/get_latest_dist_bundles_from_github.sh
+++ util/get_latest_dist_bundles_from_github.sh
@@ -5,8 +5,8 @@ cd -- "$(readlink -m -- "$BASH_SOURCE"/../..)"
 exec </dev/null
 
 RLS="$1"; shift || true
-[ -n "$RLS" ] || RLS="$(git branch 2>/dev/null |
-  sed -nre 's~^\* ~ci:~p')" || true
+BRANCH="$(git branch 2>/dev/null | sed -nre 's~^\* ~~p')" || true
+[ -n "$RLS" ] || [ -z "$BRANCH" ] || RLS="ci:$BRANCH" || true
 case "$RLS" in
   e ) RLS='ci:experimental';;
   g | '' ) RLS='ci:staging';;
@@ -14,13 +14,32 @@ case "$RLS" in
   s ) RLS='ci:stable';;
 esac
 RLS="${RLS/#ci:/auto-ci-release/}"
+[ -n "$BRANCH" ] || BRANCH="${RLS##*[/:]}" || true
 
 BUN='all_dist_bundles.zip'
 REPO='mk-pmb/anno-frontend'
-URL="https://github.com/$REPO/releases/download/$RLS/$BUN"
-ZIP="tmp.dl-$$.$BUN"
-wget --output-document="$ZIP".part -- "$URL"
-mv --verbose --no-target-directory -- "$ZIP"{.part,}
-( mkdir --parents dist && cd -- dist && unzip -jo ../"$ZIP" )
-rm --verbose -- "$ZIP"
+REPO_URL="https://github.com/$REPO"
+
+URL="$REPO_URL/actions?query=branch:$BRANCH"
+URL+='+is:in_progress'
+URL+='+is:queued'
+URL+='+is:waiting'
+
+while true; do
+  SUITES='(^|\s)id="check_suite_\d+"'
+  SUITES="$(curl --silent --show-error -- "$URL" | grep -hoPe "$SUITES" |
+    grep -oPe '\d+' | sort --general-numeric-sort --unique)"
+  SUITES="${SUITES//$'\n'/ }"
+  [ -n "$SUITES" ] || break
+  printf '\r%(%T)T %s     ' -1 "waiting for CI: $SUITES"
+  sleep 5s
+done
+echo -ne '\r'
+
+URL="$REPO_URL/releases/download/$RLS/$BUN"
+SAVE_AS="tmp.dl-$$.$BUN"
+wget --output-document="$SAVE_AS".part -- "$URL"
+mv --verbose --no-target-directory -- "$SAVE_AS"{.part,}
+( mkdir --parents dist && cd -- dist && unzip -jo ../"$SAVE_AS" )
+rm --verbose -- "$SAVE_AS"
 echo D: "Success: Updated $PWD/dist <- $URL"
